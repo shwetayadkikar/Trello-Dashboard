01c09eee34add8be3b3a414e648b89b7e43e947d
iamshweta <yadkikar.shweta@outlook.com>
2014-11-06 18:27:38 +0530

    #7 activities service layer and directive added

---
+++ TrelloDashboard/Trello Dashboard/Services/BoardService.js
@@ -0,0 +1,12 @@
+﻿angular.module("trelloDashboard").service("BoardService", ['$q', BoardService]);
+
+function BoardService($q) {
+    var self = this;
+    self.getBoardActions = getBoardActions;
+
+    function getBoardActions(boardId) {
+        return $q(function (resolve) { Trello.get("boards/" + boardId + "/actions", resolve) });
+    }
+
+    return self;
+}
