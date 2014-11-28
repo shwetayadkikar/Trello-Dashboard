11e3fea234958bc51894b888ea324d5f21752bac
iamshweta <yadkikar.shweta@outlook.com>
2014-11-28 18:41:51 +0530

    #8  done with calendar directive UI

---
+++ TrelloDashboard/Trello Dashboard/Directives/CalendarDirective.js
@@ -0,0 +1,30 @@
+﻿angular.module("trelloDashboard")
+       .directive("fullcalendar", [fullcalendarDirective]);
+
+function fullcalendarDirective() {
+    var linker = function (scope, element, attrs) {
+        element.fullCalendar({
+            events: [
+                {
+                    title: 'event1',
+                    start: '2014-11-01'
+                },
+                {
+                    title: 'event2',
+                    start: '2014-11-15',
+                    end: '2014-11-18'
+                },
+                {
+                    title: 'event3',
+                    start: '2014-11-11T12:30:00',
+                    allDay: false // will make the time show
+                }
+            ]
+        });
+    }
+
+    return {
+        restrict: 'A',
+        link: linker
+    }
+}
